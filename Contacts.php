bb94c48d2dbb1207d18a070ab1507a647990e713
DianaKhikmatova <d.m.khikmatova@gmail.com>
2016-12-20 01:35:03 +0200

    with css for main page

---
+++ Contacts.php
@@ -0,0 +1,18 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 center little-down">
+			<h1>Contacts</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<p>Book and magazine store "Noosphere" works 7 days per day 365 days per year!
+			We works at weekdays, weekends and holidays for you to give you an opportunity 
+			to have a nice time. Our site takes orders around the clock.
+			Each good will be available for you in the within the shortest possible period 
+			of time it is in the store.</p>
+			<p><span class="bold">Email: </span>noosphere@gmail.com</p>
+			<p><span class="bold">Phone: </span>096 79 13 719</p>
+		</div>
+	</div>
+</div>
