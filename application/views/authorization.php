932856c23f8f4be90717f88a7ac4ecdfe92031ad
DianaKhikmatova <d.m.khikmatova@gmail.com>
2016-12-21 00:28:59 +0200

    authorization and registration

---
+++ application/views/authorization.php
@@ -0,0 +1,14 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-4"></div>
+		<div class="col-md-4">
+			<div class="registration center">
+				<h2>Authorization</h2>
+				<input type="text" class="form-control" id="email" name="email" required placeholder="Email"><br/>
+				<input type="password" class="form-control" id="password" name="password" required placeholder="Password"><br/>
+				<button type="button" class="btn btn-danger right-btn" id="authorize">Authorize</button>
+			</div>
+		</div>
+		<div class="col-md-4"></div>
+	</div>
+</div>
--- application/views/header.php
+++ application/views/header.php
@@ -4,21 +4,15 @@
       <a class="navbar-brand" href="/home">Noosphere</a>
     </div>
     <ul class="nav navbar-nav">
-<<<<<<< HEAD
       <li><a href="#">Home</a></li>
       <li class="dropdown">
         <a class="dropdown-toggle" data-toggle="dropdown" href="#">Products
         <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="admin_book.html">Books</a></li>
-          <li><a href="admin_magazine.html">Magazines</a></li>
+          <li><a href="/books">Books</a></li>
+          <li><a href="/magazinesl">Magazines</a></li>
         </ul>
       </li>
-=======
-      <li><a href="/home">Home</a></li>
-      <li><a href="/books">Books</a></li>
-      <li><a href="/magazines">Magazines</a></li>
->>>>>>> e78c9147f0dda40e02a790e5b1cbd459a10ddf8f
       <li><a href="/contacts">Contacts</a></li>
     </ul>
 	<ul class="nav navbar-nav navbar-right">
