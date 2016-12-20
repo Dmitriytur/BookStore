efbadd1687ca15e9d5609f98e65a2a416329a484
DianaKhikmatova <d.m.khikmatova@gmail.com>
2016-12-20 22:56:52 +0200

    Header for admin and adding of books and magazines

--- application/views/header_admin.php
+++ application/views/header_admin.php
@@ -4,17 +4,9 @@
       <a class="navbar-brand" href="/home">Noosphere</a>
     </div>
     <ul class="nav navbar-nav">
-      <li><a href="#">Home</a></li>
-      <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Products
-        <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="admin_book.html">Books</a></li>
-          <li><a href="admin_magazine.html">Magazines</a></li>
-        </ul>
-      </li>
-      <li><a href="/contacts">Contacts</a></li>
-    </ul>
+      <li><a href="#">Books</a></li>
+	  <li><a href="#">Magazines</a></li>
+	</ul>
 	<ul class="nav navbar-nav navbar-right">
       <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
       <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
